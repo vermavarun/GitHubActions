62cb09c6bd43d30ce9847ed25433b951f2f108f9
Varun Verma <vermavarun@outlook.com>
2022-09-06 19:46:20 +0530

    Update list-files.ps1

--- list-files.ps1
+++ list-files.ps1
@@ -1,4 +1,4 @@
-$files = Get-ChildItem . *.* -rec -exclude rename-files.ps1
+$files = Get-ChildItem ../ *.* -rec -exclude rename-files.ps1
 foreach ($file in $files) {
         Write-Host $file.FullName    
 }
